f7f03947d2a48c6c9297f2277c04df35475366c8
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-20 17:01:29 -0700

    customer avatars

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -29,8 +29,8 @@ module.exports = {
    
     const avatars = {
     
-      Female: ['https://i.imgur.com/nlhLi3I.png','https://i.imgur.com/z5LNkkB.png','https://i.imgur.com/v0JXau2.png','https://i.imgur.com/lRUnDgU.png', 'https://i.imgur.com/3GvwNBf.png'],
-      Male: ['https://i.imgur.com/73hZDYK.png','https://i.imgur.com/5fUVPRP.png','https://i.imgur.com/DVpDmdR.png','https://i.imgur.com/2WZtOD6.png', 'https://i.imgur.com/ilT4JDe.png']
+      Female: ['/images/avatars/woman.png', '/images/avatars/woman2.png', '/images/avatars/woman3.png', '/images/avatars/woman4.png'],
+      Male: ['/images/avatars/boy.png', '/images/avatars/man2.png', '/images/avatars/man2.png', '/images/avatars/man3.png', '/images/avatars/man4.png']
     
     };
     
